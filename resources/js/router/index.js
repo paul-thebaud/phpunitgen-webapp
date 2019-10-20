74dc3a5422d44b0cdf2ddd150800a21ded230508
Paul Thébaud <paul.thebaud29@gmail.com>
2019-10-20 22:21:46 +0200

    Vue router scroll behavior

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -10,6 +10,13 @@ Vue.use(Router);
 
 export default new Router({
     mode: 'history',
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        }
+
+        return { x: 0, y: 0 };
+    },
     routes: [
         { path: '/', name: 'home', component: Home },
         { path: '/tool', name: 'tool', component: Tool },
