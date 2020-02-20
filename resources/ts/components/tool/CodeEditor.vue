076a3c9b42c8444371e3ae01cbbef92cfde5c84d
Paul Thébaud <paul.thebaud29@gmail.com>
2020-02-20 13:40:39 +0100

    Fix theme switching on editor

--- resources/ts/components/tool/CodeEditor.vue
+++ resources/ts/components/tool/CodeEditor.vue
@@ -12,9 +12,9 @@
     import "codemirror/mode/php/php";
     import Vue from "vue";
     import { codemirror } from "vue-codemirror";
-    import { Component, Inject, Prop } from "vue-property-decorator";
-    import { ThemeI } from "@/container/contracts/themeI";
-    import { TYPES } from "@/container/types";
+    import { Component, Prop } from "vue-property-decorator";
+    import { Theme } from "@/container/concerns/theme";
+    import { State } from "vuex-class";
 
     @Component({
         components: {
@@ -22,9 +22,6 @@
         },
     })
     export default class CodeEditor extends Vue {
-        @Inject(TYPES.Theme)
-        protected readonly theme!: ThemeI;
-
         @Prop(String)
         protected readonly code!: string;
 
@@ -37,16 +34,21 @@
         @Prop({ type: String, default: "application/x-httpd-php" })
         protected readonly mode!: string;
 
-        protected codeMirrorOptions = {
-            viewportMargin: Infinity,
-            theme: this.theme.currentTheme.getCodeMirror(),
-            mode: this.mode,
-            indentUnit: 4,
-            tabSize: 4,
-            lineNumbers: true,
-            line: true,
-            readOnly: this.readOnly,
-        };
+        @State
+        protected readonly theme!: Theme;
+
+        protected get codeMirrorOptions(): object {
+            return {
+                viewportMargin: Infinity,
+                theme: this.theme.getCodeMirror(),
+                mode: this.mode,
+                indentUnit: 4,
+                tabSize: 4,
+                lineNumbers: true,
+                line: true,
+                readOnly: this.readOnly,
+            };
+        }
 
         protected handleInput(code: string): void {
             this.$emit("input", code);
