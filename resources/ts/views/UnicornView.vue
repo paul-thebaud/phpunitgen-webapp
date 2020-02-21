274698263773a5943960cd4958ea861af601f609
Paul Thébaud <paul.thebaud29@gmail.com>
2020-02-21 13:19:48 +0100

    Update typo

--- resources/ts/views/UnicornView.vue
+++ resources/ts/views/UnicornView.vue
@@ -1,8 +1,11 @@
 <template>
   <BContainer class="my-5 h-100 d-flex align-items-center justify-content-center">
-    <h1>
-      <code class="font-weight-bold">$this->assertTrue($unicorn->exists());</code>
-    </h1>
+    <span class="d-none d-lg-block font-weight-bold h2">
+      <code>$this->assertTrue($unicorn->exists());</code>
+    </span>
+    <span class="d-block d-lg-none font-weight-bold h5">
+      <code>$this->assertTrue($unicorn->exists());</code>
+    </span>
   </BContainer>
 </template>
 
